9566fee3ef1bb5cf6e093a3cf0903bc6dee06736
Brandon Wood <bwood@gci.com>
2020-11-05 15:55:55 -0900

    use FontAwesome for weather icons (fixes #3)

--- MagicMirror/Models/Weather.cs
+++ MagicMirror/Models/Weather.cs
@@ -72,7 +72,7 @@ namespace MagicMirror.Models
         public TempRange FeelsLike { get; set; }
     }
 
-    //More info: https://openweathermap.org/weather-conditions
+    // More info: 
     public class WeatherCondition
     {
         [JsonProperty("main")]
@@ -86,6 +86,58 @@ namespace MagicMirror.Models
 
         public string IconUrl => $"/img/weather/{Icon}.png";
         public string IconLargeUrl => $"/img/weather/{Icon}.png";
+
+        // We need to map the weather condition codes used by OpenWeatherMap, to 
+        // the CSS class names used by our weather icons.
+        // More info:
+        //   https://openweathermap.org/weather-conditions
+        //   https://fontawesome.com/icons?d=gallery&c=weather&m=free
+        public string IconCssClass => ConditionIcons[Icon];
+
+        private Dictionary<string, string> ConditionIcons = new Dictionary<string, string>
+            {
+                { "01d", "fas fa-sun" },
+                { "01n", "far fa-moon" },
+                { "02d", "fas fa-cloud-sun" },
+                { "02n", "fas fa-cloud-moon" },
+                { "03d", "fas fa-cloud" },
+                { "03n", "fas fa-cloud" },
+                { "04d", "fas fa-cloud-sun" },
+                { "04n", "fas fa-cloud-moon" },
+                { "09d", "fas fa-cloud-showers-heavy" },
+                { "09n", "fas fa-cloud-showers-heavy" },
+                { "10d", "fas fa-cloud-rain" },
+                { "10n", "fas fa-cloud-rain" },
+                { "11d", "fas fa-bolt" },
+                { "11n", "fas fa-bolt" },
+                { "13d", "far fa-snowflake" },
+                { "13n", "far fa-snowflake" },
+                { "50d", "fas fa-smog" },
+                { "50n", "fas fa-smog" },
+            };
+
+        //   https://erikflowers.github.io/weather-icons/
+        //private Dictionary<string, string> ConditionIcons = new Dictionary<string, string>
+        //    {
+        //        { "01d", "wi wi-day-sunny" },
+        //        { "01n", "wi wi-night-clear" },
+        //        { "02d", "wi wi-day-sunny-overcast" },
+        //        { "02n", "wi wi-night-alt-partly-cloudy" },
+        //        { "03d", "wi wi-cloud" },
+        //        { "03n", "wi wi-cloud" },
+        //        { "04d", "wi wi-day-cloudy" },
+        //        { "04n", "wi wi-night-alt-cloudy" },
+        //        { "09d", "wi wi-raindrops" },
+        //        { "09n", "wi wi-raindrops" },
+        //        { "10d", "wi wi-raindrops" },
+        //        { "10n", "wi wi-raindrops" },
+        //        { "11d", "wi wi-thunderstorm" },
+        //        { "11n", "wi wi-thunderstorm" },
+        //        { "13d", "wi wi-snowflake-cold" },
+        //        { "13n", "wi wi-snowflake-cold" },
+        //        { "50d", "wi wi-fog" },
+        //        { "50n", "wi wi-fog" },
+        //    };
     }
 
     public class TempRange
