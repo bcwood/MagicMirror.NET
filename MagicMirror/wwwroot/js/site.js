f422ad31142648639dbabdebe2e02e6887a003db
Brandon Wood <bwood@gci.com>
2020-11-19 14:53:27 -0900

    allow specifying refresh rate with h/m/s suffix

--- MagicMirror/wwwroot/js/site.js
+++ MagicMirror/wwwroot/js/site.js
@@ -1,7 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-$(document).ready(function (e) {
+﻿$(document).ready(function (e) {
 	$(".partialContents").each(function (index, item) {
 		var url = $(item).data("url");
 		var refresh = $(item).data("refresh");
@@ -15,16 +12,22 @@ $(document).ready(function (e) {
 			return;
 		}
 
-		refresh = refresh * 1000;
+		var unit = refresh.slice(-1);
+		refresh = refresh.substring(0, refresh.length - 1);
+
+		if (unit == "h") { // hours to ms
+			refresh = refresh * 60 * 60 * 1000;
+		}
+		else if (unit == "m") { // minutes to ms
+			refresh = refresh * 60 * 1000;
+		}
+		else { // seconds to ms
+			refresh = refresh * 1000;
+		}
+
+		//alert("url: " + url + "\nrefresh: " + refresh);
 
 		$(item).load(url);
 		setInterval(function() { $(item).load(url); }, refresh);
-
-		//reloadContent(item, url, refresh);
 	});
-
-	function reloadContent(item, url, timeout) {
-		$(item).load(url);
-		setTimeout(reloadContent(item, url, timeout), timeout);
-	}
 });
